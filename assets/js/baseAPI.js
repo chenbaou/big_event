a42ea0efe3bd9b114b22d76b36dd7ec7960e09a1
chenbaoyu <15503760050@163.com>
2021-01-05 00:04:43 +0800

    完成了个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,7 @@
 $.ajaxPrefilter(function (options) {
     // console.log(options.url);
     //发起真正的ajax请求前，统一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
     //统一为有权限的接口，设置headers请求头
     if (options.url.indexOf('/my/') !== -1) {
         options.headers = {
@@ -10,14 +10,14 @@ $.ajaxPrefilter(function (options) {
         }
     }
     //全局挂载complete回调函数
-    options.complete= function (res) {
-        console.log(res);
+    options.complete = function (res) {
+        // console.log(res);
         //在complete回调函数中，可以通过res.responseJSON拿到服务器响应回来的数据
         if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
             //强制清空token
             localStorage.removeItem('token');
             //跳转到登录页
-            location.href='/login.html'
+            location.href = '/login.html'
         }
     }
 })
